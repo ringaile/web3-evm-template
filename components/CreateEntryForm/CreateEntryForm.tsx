74ee6552815dd6684088837424e5b9dc442dab1f
Ringaile Valiaugaite <ringaile@gmail.com>
2022-03-27 15:26:04 +0100

    added arweave txs

--- components/CreateEntryForm/CreateEntryForm.tsx
+++ components/CreateEntryForm/CreateEntryForm.tsx
@@ -60,17 +60,14 @@ const CreateEntryForm = (): ReactElement => {
         if (provider && contract) {
           const data = createJsonMetaData(values);
 
-          // For Step 3: Create Post
-          // Submit Arweave transaction
-          // Use axios to post data and address to api/arweave/post endpoint.
-          // This request should return transactionId
+          const response = await axios.post(routes.api.arweave.post, {
+            data,
+            address,
+          });
+          const transactionId = response.data;
+          console.log('transactionId: ', transactionId);
 
-          // Stop here when you complete Step 3 ^^^^
-
-          // For Step 6: Mint NFT
-          // Get signer and connect it to smart contract
-          // More information can be found here: https://docs.ethers.io/v5/getting-started/#getting-started--writing
-
+          let rec = true;
           if (rec) {
             mutate(routes.api.arweave.search());
             alert('Entry created successfully');
--- pages/api/arweave/entry.ts
+++ pages/api/arweave/entry.ts
@@ -11,22 +11,17 @@ export default async function (
   try {
     const {data, address} = req.body;
 
-    // Initialize wallet using ARWEAVE_WALLET environmental variable (Tip: Use JSON.parse)
+    const wallet = JSON.parse(process.env.ARWEAVE_WALLET as string);
+    const transaction = await arweave.createTransaction({data: data}, wallet);
 
-    // Create Arweave transaction passing in data. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
+    transaction.addTag('App-Name', process.env.APP_NAME as string);
+    transaction.addTag('Content-Type', 'application/json');
+    transaction.addTag('Address', address);
 
-    // Add tags:
-    // - App-Name - APP_NAME environmental variable
-    // - Content-Type - Should be application/json
-    // - Address - Address of a user
-    // Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
+    const r = await arweave.transactions.sign(transaction, wallet);
+    const result = await arweave.transactions.post(transaction);
 
-    // Sign Arweave transaction with your wallet. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
-
-    // Post Arweave transaction. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
-
-    // Return transaction id
-    res.status(200).json('<Transaction ID>');
+    res.status(200).json(transaction.id);
   } catch (error) {
     console.log('ERROR', error);
     const errorMessage =
