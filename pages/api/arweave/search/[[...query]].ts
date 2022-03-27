eb5e49df03262fbf3c70e0cef9e3fb3954070d2f
Ringaile Valiaugaite <ringaile@gmail.com>
2022-03-27 15:30:57 +0100

    display entries

--- pages/api/arweave/search/[[...query]].ts
+++ pages/api/arweave/search/[[...query]].ts
@@ -24,21 +24,21 @@ export default async function (
   try {
     const {query} = _req.query;
 
-    // Initialize ArDB
-    // More information about ArDB can be found here: https://www.npmjs.com/package/ardb
+  const ardb = new ArDB(arweave);
 
-    // Retrieve searchAddress
-    
-    // Build tags
+  const searchAddress = query && query[0];
 
-    // Search for transaction withs App-Name and Address (optional) tags
-    // More information can be found here: https://www.npmjs.com/package/ardb
-    const txs = [];
+  const tags = [{name: 'App-Name', values: [process.env.APP_NAME as string]}];
+  if (searchAddress) {
+    tags.push({name: 'Address', values: [searchAddress]});
+  }
 
-    const promises = txs.map((tx: any) => getData(tx._id));
-    const data = await Promise.all(promises);
+  const txs = await ardb.search('transactions').tags(tags).limit(10).find();
 
-    res.status(200).json(data);
+  const promises = txs.map((tx: any) => getData(tx._id));
+  const data = await Promise.all(promises);
+
+  res.status(200).json(data);
   } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : 'Unknown Error';
