b0c70cd0b6224ec0332fa23580da799947f2b058
Ringaile Valiaugaite <ringaile@gmail.com>
2022-04-24 12:14:31 +0100

    added increment of listing

--- web3/contracts/MirrorMarketplace.sol
+++ web3/contracts/MirrorMarketplace.sol
@@ -28,6 +28,7 @@ contract MirrorMarketplace {
     function list(IERC721 _token, uint _tokenId, uint _price) public {
         require(_price > 0, "Invalid price");
         listings[listingId] = Listing(_token, msg.sender, _tokenId, _price, block.timestamp, ListingStatus.Listed);
+        listingId += 1;
         _token.transferFrom(msg.sender, address(this), _tokenId);
         emit Listed(listingId, address(_token), msg.sender, _tokenId, _price);
     }
